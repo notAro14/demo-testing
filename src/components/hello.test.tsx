648a2247333822797cdf98d87761abe2ca36a5f3
Aro Andriamaro <47597252+notAro14@users.noreply.github.com>
2023-09-04 00:09:39 +0200

    test: add demo testing of demo comp

--- src/components/hello.test.tsx
+++ src/components/hello.test.tsx
@@ -1,9 +1,21 @@
-import { test } from "vitest";
+import { expect, it } from "vitest";
+
+import { server, rest } from "../test/mocks";
+import { render, screen, waitForElementToBeRemoved } from "../test/utils";
 
 import Hello from "./hello";
-import { render, screen } from "../test/utils";
 
-test("should sum two positive numbers", () => {
+const MOCKED_USERS = [
+  { name: "Agent Smith", email: "null@error.matrix", id: "smith" },
+];
+
+it("should retrieve a list of users", async function () {
+  server.use(
+    rest.get("https://jsonplaceholder.typicode.com/users", (_req, res, ctx) => {
+      return res(ctx.json(MOCKED_USERS));
+    }),
+  );
   render(<Hello />);
-  screen.debug();
+  await waitForElementToBeRemoved(screen.getByRole("alert"));
+  expect(screen.getAllByRole("listitem")).toHaveLength(MOCKED_USERS.length);
 });
